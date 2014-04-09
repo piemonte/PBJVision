0ea1187e3bc32b96ac171908a316d758fe254c2b
Patrick Piemonte <patrick@diy.org>
2014-04-09 15:01:42 -0700

    PBJGLProgram, added comments

--- Source/PBJGLProgram.h
+++ Source/PBJGLProgram.h
@@ -10,10 +10,12 @@
 #import <OpenGLES/ES2/gl.h>
 #import <OpenGLES/ES2/glext.h>
 
+// common attribute names
 extern NSString * const PBJGLProgramAttributeVertex;
 extern NSString * const PBJGLProgramAttributeTextureCoord;
 extern NSString * const PBJGLProgramAttributeNormal;
 
+// inspired by Jeff LaMarche, https://github.com/jlamarche/iOS-OpenGLES-Stuff
 @interface PBJGLProgram : NSObject
 
 - (id)initWithVertexShaderName:(NSString *)vertexShaderName fragmentShaderName:(NSString *)fragmentShaderName;
