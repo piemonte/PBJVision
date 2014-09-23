68040349204a95792a2ec3a37c2d9fc956e6bbc7
Ben Cherry <bcherry@gmail.com>
2014-09-23 11:33:52 -0700

    Remove observer on _captureOutputPhoto during dealloc

--- Source/PBJVision.m
+++ Source/PBJVision.m
@@ -894,6 +894,8 @@ typedef void (^PBJVisionBlock)();
     // capture device notifications
     [notificationCenter removeObserver:self name:AVCaptureDeviceSubjectAreaDidChangeNotification object:nil];
 
+    [_captureOutputPhoto removeObserver:self forKeyPath:@"capturingStillImage"];
+
     _captureOutputPhoto = nil;
     _captureOutputAudio = nil;
     _captureOutputVideo = nil;
